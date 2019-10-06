f2578d0fb0e008c0f9bd20fd97e0bba45a64eaed
Mitch <MitchellSeigel@creighton.edu>
2019-10-06 18:42:49 -0500

    Initial commit

---
+++ forms/membership/membership.js
@@ -0,0 +1,13 @@
+
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+btnSubmit2.onclick=function(){
+  name = inptFirstName.value
+  
+  if (name == members) {
+    lblMessage2.value = "You are already a member."
+  } else {   
+    lblMessage2.value = "Your are not currently a member but are being added to the member roster as we speak."
+    members.push(name)
+}  
+}
