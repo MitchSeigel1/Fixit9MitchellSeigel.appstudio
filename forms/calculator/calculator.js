c91107e9288fee5f38e32deca1a5e13417649fde
Mitch <MitchellSeigel@creighton.edu>
2019-10-08 21:24:55 -0500

    second

---
+++ forms/calculator/calculator.js
@@ -0,0 +1,15 @@
+
+btnEqualls.onclick=function(){
+  let num1 = Number(inptNum1.value)
+  let num2 = Number(inptNum2.value)
+  let opperator = (inptOpp.value)
+if (opperator === '+') {
+  lblAnswer.value = num1 + num2
+} else if (opperator === '-') {
+  lblAnswer.value = num1 - num2
+} else if (opperator === '*') {
+  lblAnswer.value = num1 * num2
+  } else if (opperator === '/') {
+  lblAnswer.value = num1 / num2
+  }
+}
--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,14 +1,14 @@
-/*
+
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
 
 btnSubmit2.onclick=function(){
   name = inptFirstName.value
   
-  if (name == members) {
+
+  if (name === "Mary" || name === "Sam" || name === "Joey" || name === "Paul" || name === "Mike" || name === "Char" || name === "Jenny" || name === "Kennedy" || name === "Kaden" || name === "Kurt" || name === "Anne") {
     lblMessage2.value = "You are already a member."
   } else {   
     lblMessage2.value = "Your are not currently a member but are being added to the member roster as we speak."
     members.push(name)
   }  
 }
-*?
